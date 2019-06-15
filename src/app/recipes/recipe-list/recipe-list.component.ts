bada3926ff174a099f4bec711ca6d19b6174cbdc
LukasFrm <lukasfreimonas@gmail.com>
2019-06-16 00:42:31 +0300

    Recipe & Ingredient models added, some right page side functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+  new Recipe ("Lithuanian Potato-Meat Dumplings (Cepelinai)", "So named because their shape resembles that of a Zeppelin airship, cepelinai are typically around 10–20 cm long, although the size depends on where they are made: in the western counties of Lithuania cepelinai are made bigger than in the east. In Samogitia cepelinai are called 'kleckā'.", "https://www.seforeceptai.lt/upload/2013/06/580_Cepelinai_su.jpg")
+
+  ]
 
   constructor() { }
 
